07fcfddf3c84d1b61c16ce6572c09654db65dd77
walsoup <walidelonk@gmail.com>
2026-07-21 15:38:51 +0100

    fix: add classes transform to babel config to target ES5 functions

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      '@babel/plugin-transform-classes',
       '@babel/plugin-transform-class-properties',
       '@babel/plugin-transform-private-methods',
       '@babel/plugin-transform-private-property-in-object',
