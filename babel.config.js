83269a5bf55b146045971c0ae4799d742b9f8991
walsoup <walidelonk@gmail.com>
2026-07-21 17:25:52 +0100

    fix: use hermes-v0 profile in babel-preset-expo to transpile classes and private fields

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,15 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          unstable_transformProfile: 'hermes-v0',
+        },
+      ],
+    ],
     plugins: [
-      '@babel/plugin-transform-classes',
-      '@babel/plugin-transform-class-properties',
-      '@babel/plugin-transform-private-methods',
-      '@babel/plugin-transform-private-property-in-object',
       'react-native-reanimated/plugin',
     ],
   };
